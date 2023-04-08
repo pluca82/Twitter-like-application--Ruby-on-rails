e78d591481818f4fae5d93997e0c8b5f6b2d1bfe
Luca Popa <pluca1660@gmail.com>
2023-04-08 15:36:15 +0200

    impletent advanced login

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -2,15 +2,32 @@ module SessionsHelper
     def log_in(user)
         session[:user_id] = user.id
     end 
-    def current_user
-        if session[:user_id]
-            @current_user ||= User.find_by(id: session[:user_id])
-        end
+    def remember(user)
+        user.remember
+        cookies.permanent.encrypted[:user_id] = user.id
+        cookies.encrypted[:remember_token] = user.remember_token
     end 
-    def logged_in
-        !current_user.nil?
+    def current_user
+        if (user_id = session[:user_id])
+            @current_user ||= User.find_by(id: user_id)
+        elsif (user_id = cookies.encrypted[:user_id])
+            user = User.find_by(id: user_id)
+        if user && user.authenticated?(cookies[:remember_token])
+            log_in user
+            @current_user = user 
+        end 
     end
-    def logged_out
+    end 
+    def forget(user)
+        user.forget
+        cookies.delete(:remember_token)
+        cookies.delete(:user_id)
+    end 
+    def logged_in?
+        !current_user.nil?
+        end
+    def log_out
+        forget(current_user)
         session.delete(:user_id)
         current_user = nil 
     end 
