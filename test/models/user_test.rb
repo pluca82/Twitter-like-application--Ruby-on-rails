740d37927c22c1dfbe1e32a51b12fe234b108e5f
Luca Popa <pluca1660@gmail.com>
2023-04-21 15:10:23 +0200

    Added option to follow users

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -61,5 +61,15 @@ test "verification should reject invalid email adresses" do
   end
   test "authenticated? should return false for a user with nil digest" do 
     assert_not @user.authenticated?(:remember, '')
-  end 
+  end
+  
+  test "should follow and unfollow a user" do
+    michael = users(:michael)
+    archer = users(:archer)
+    assert_not michael.following?(archer)
+    michael.follow(archer)
+    assert michael.following?(archer)
+    michael.unfollow(archer)
+    assert_not michael.following?(archer)
+  end
 end
